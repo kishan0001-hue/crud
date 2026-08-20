40533b8098dbf5248775bc01b80c7977ef0a64e6
Aditya <24amtics034@gmail.com>
2026-08-20 18:02:03 +0530

    create

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import blog
+from .serializer import blogserializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
 
 # Create your views here.
+
+@api_view (['POST'])
+def blog_create (request):
+
+    Value = blogserializer( data = request.data)
+    if Value.is_valid:
+        Value.save()
+        return Response( Value.data , status= status.HTTP_200_OK)
+    return Response(Value.error , status=status.HTTP_400_BAD_REQUEST)
+
+
